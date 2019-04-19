348f1201d420ce8f498e6d3c60d1f5af226cdb5d
Евгений Гаврилов <e.gavrilov@superjob.ru>
2019-04-19 13:48:45 +0300

    first commit

---
+++ packages/core/core.ts
@@ -0,0 +1,50 @@
+import {cn} from '@bem-react/classname';
+
+//TODO: set types
+
+export function withBemMod(blockName: string, mod: any, modProps: any, enhance: any) {
+    return function WithBemMod(WrappedComponent: any) {
+        let ModifiedComponent: any;
+
+        return {
+            functional: true,
+            render(h: any, context: any) {
+                const entity = cn(blockName);
+                const props = context.props;
+                const isMatched = (key: string) => (props)[key] === mod[key];
+                const isStarMatched = (key: string) => mod[key] === '*' &&
+                    Boolean((props)[key]);
+
+                if (Object.keys(mod).every(key => isMatched(key) || isStarMatched(key))) {
+                    let mods = Object.keys(mod).reduce((acc: any, key: string) => {
+                        if (mod[key] !== '*') acc[key] = mod[key];
+
+                        return acc;
+                    }, {});
+
+                    const modifierClassName = entity(mods).replace(`${entity()} `, '');
+
+                    context.data.class = {
+                        ...context.data.class,
+                        [modifierClassName]: true,
+                    };
+
+                    if (enhance !== undefined) {
+                        if (ModifiedComponent === undefined) {
+                            ModifiedComponent = enhance(WrappedComponent);
+                        }
+                    } else {
+                        ModifiedComponent = WrappedComponent;
+                    }
+
+                    return h(ModifiedComponent, {
+                        props: props,
+                        ...context.data,
+                    }, context.children);
+                }
+
+                return h(WrappedComponent, context.data, context.children);
+            },
+        };
+    };
+}
