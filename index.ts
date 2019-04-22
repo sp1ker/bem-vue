b65a5b7f09431504166ae75f82b814aec56b9428
Sp1ker <Sp1ker@users.noreply.github.com>
2019-04-22 22:30:38 +0300

    create npm package

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 //TODO: dev or prod
 // ts to js
-import { compose as compose1, withBemMod as withBemMod1 } from './src/core';
+import { compose as compose1, withBemMod as withBemMod1 } from './src';
 
 export const compose = compose1;
 export const withBemMod = withBemMod1;
