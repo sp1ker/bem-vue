9f17edd61393d5a57d718e666e8c2b23e0a44aa6
Евгений Гаврилов <e.gavrilov@superjob.ru>
2019-04-25 17:29:43 +0300

    add injecting props to functional components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
-import { cn } from '@bem-react/classname';
+import {ClassNameFormatter, cn} from '@bem-react/classname';
+import {CreateElement} from "vue";
 
 //TODO: set types
+
+function composeMods(
+    h: CreateElement,
+    mod: any,
+    entity: ClassNameFormatter,
+    context: any,
+    enhance: any,
+    ModifiedComponent: any,
+    WrappedComponent: any,
+    props: any) {
+    let mods = Object.keys(mod).reduce((acc: any, key: string) => {
+        if (mod[key] !== '*') acc[key] = mod[key];
+
+        return acc;
+    }, {});
+
+    const modifierClassName = entity(mods).replace(`${entity()} `, '');
+
+    context.data.class = {
+        ...context.data.class,
+        [modifierClassName]: true,
+    };
+
+    if (enhance !== undefined) {
+        if (ModifiedComponent === undefined) {
+            ModifiedComponent = enhance(WrappedComponent);
+        }
+    } else {
+        ModifiedComponent = WrappedComponent;
+    }
+
+    return h(ModifiedComponent, {
+        props: props,
+        ...context.data,
+    }, context.children);
+}
 
 export function withBemMod(blockName: string, mod: any, enhance?: any) {
     return function WithBemMod(WrappedComponent: any) {
@@ -8,39 +45,19 @@ export function withBemMod(blockName: string, mod: any, enhance?: any) {
 
         return {
             functional: true,
-            render(h: any, context: any) {
-                const entity = cn(blockName);
+            props: Object.keys(mod).reduce((mods: { [key: string]: {} }, modName: string) => {
+                mods[modName] = {};
+                return mods;
+            }, {}),
+            render(h: CreateElement, context: any) {
+                const entity: ClassNameFormatter = cn(blockName);
                 const props = context.props;
                 const isMatched = (key: string) => (props)[key] === mod[key];
                 const isStarMatched = (key: string) => mod[key] === '*' &&
-                  Boolean((props)[key]);
+                    Boolean((props)[key]);
 
                 if (Object.keys(mod).every(key => isMatched(key) || isStarMatched(key))) {
-                    let mods = Object.keys(mod).reduce((acc: any, key: string) => {
-                        if (mod[key] !== '*') acc[key] = mod[key];
-
-                        return acc;
-                    }, {});
-
-                    const modifierClassName = entity(mods).replace(`${entity()} `, '');
-
-                    context.data.class = {
-                        ...context.data.class,
-                        [modifierClassName]: true,
-                    };
-
-                    if (enhance !== undefined) {
-                        if (ModifiedComponent === undefined) {
-                            ModifiedComponent = enhance(WrappedComponent);
-                        }
-                    } else {
-                        ModifiedComponent = WrappedComponent;
-                    }
-
-                    return h(ModifiedComponent, {
-                        props: props,
-                        ...context.data,
-                    }, context.children);
+                    return composeMods(h, mod, entity, context, enhance, ModifiedComponent, WrappedComponent, props);
                 }
 
                 return h(WrappedComponent, context.data, context.children);
@@ -50,5 +67,15 @@ export function withBemMod(blockName: string, mod: any, enhance?: any) {
 }
 
 export function compose(...funcs: any[]) {
-    return funcs.reduce((a, b) => (...args: any[]) => a(b(...args)), (arg: any) => arg);
+    return funcs.reduce(
+        (a, b) => {
+            return (...args: any[]) => {
+                return a(b(...args));
+            }
+        },
+        (arg: any) => {
+            console.log('last-function', arg);
+            return arg
+        }
+    );
 }
