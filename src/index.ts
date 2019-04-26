a54f51ad70d4206e0c47c9250525e1b285361979
Sp1ker <Sp1ker@users.noreply.github.com>
2019-04-26 22:58:10 +0300

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,11 @@
-import {ClassNameFormatter, cn} from '@bem-react/classname';
-import {CreateElement} from "vue";
+import { ClassNameFormatter, cn, NoStrictEntityMods } from '@bem-react/classname';
+import { ComponentOptions, CreateElement, FunctionalComponentOptions, RenderContext } from 'vue';
+import { Vue } from 'vue/types/vue';
 
 //TODO: set types
+export type Enhance<V extends Vue> = (WrappedComponent: ComponentOptions<V>) => FunctionalComponentOptions;
 
-function composeMods(
-    h: CreateElement,
-    mod: any,
-    entity: ClassNameFormatter,
-    context: any,
-    enhance: any,
-    ModifiedComponent: any,
-    WrappedComponent: any,
-    props: any) {
-    let mods = Object.keys(mod).reduce((acc: any, key: string) => {
-        if (mod[key] !== '*') acc[key] = mod[key];
-
-        return acc;
-    }, {});
-
-    const modifierClassName = entity(mods).replace(`${entity()} `, '');
-
-    context.data.class = {
-        ...context.data.class,
-        [modifierClassName]: true,
-    };
-
-    if (enhance !== undefined) {
-        if (ModifiedComponent === undefined) {
-            ModifiedComponent = enhance(WrappedComponent);
-        }
-    } else {
-        ModifiedComponent = WrappedComponent;
-    }
-
-    return h(ModifiedComponent, {
-        props: props,
-        ...context.data,
-    }, context.children);
-}
-
-export function withBemMod(blockName: string, mod: any, enhance?: any) {
+export function withBemMod<V extends Vue>(blockName: string, mod: NoStrictEntityMods, enhance?: Enhance<V>) {
     return function WithBemMod(WrappedComponent: any) {
         let ModifiedComponent: any;
 
@@ -49,15 +15,39 @@ export function withBemMod(blockName: string, mod: any, enhance?: any) {
                 mods[modName] = {};
                 return mods;
             }, {}),
-            render(h: CreateElement, context: any) {
+            render(h: CreateElement, context: RenderContext) {
                 const entity: ClassNameFormatter = cn(blockName);
                 const props = context.props;
                 const isMatched = (key: string) => (props)[key] === mod[key];
                 const isStarMatched = (key: string) => mod[key] === '*' &&
-                    Boolean((props)[key]);
+                  Boolean((props)[key]);
 
                 if (Object.keys(mod).every(key => isMatched(key) || isStarMatched(key))) {
-                    return composeMods(h, mod, entity, context, enhance, ModifiedComponent, WrappedComponent, props);
+                    let mods = Object.keys(mod).reduce((acc: any, key: string) => {
+                        if (mod[key] !== '*') acc[key] = mod[key];
+
+                        return acc;
+                    }, {});
+
+                    const modifierClassName = entity(mods).replace(`${entity()} `, '');
+
+                    context.data.class = {
+                        ...context.data.class,
+                        [modifierClassName]: true,
+                    };
+
+                    if (enhance !== undefined) {
+                        if (ModifiedComponent === undefined) {
+                            ModifiedComponent = enhance(WrappedComponent);
+                        }
+                    } else {
+                        ModifiedComponent = WrappedComponent;
+                    }
+
+                    return h(ModifiedComponent, {
+                        props: props,
+                        ...context.data,
+                    }, context.children);
                 }
 
                 return h(WrappedComponent, context.data, context.children);
@@ -68,14 +58,14 @@ export function withBemMod(blockName: string, mod: any, enhance?: any) {
 
 export function compose(...funcs: any[]) {
     return funcs.reduce(
-        (a, b) => {
-            return (...args: any[]) => {
-                return a(b(...args));
-            }
-        },
-        (arg: any) => {
-            console.log('last-function', arg);
-            return arg
-        }
+      (a, b) => {
+          return (...args: any[]) => {
+              return a(b(...args));
+          };
+      },
+      (arg: any) => {
+          console.log('last-function', arg);
+          return arg;
+      },
     );
 }
